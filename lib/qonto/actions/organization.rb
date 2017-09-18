3d8ee3013e866f0b5c256d8d8bffd5571ff85626
Sébastien Charrier <scharrier@gmail.com>
2017-09-18 09:47:32 +0200

    Move BankAccounts generation to the action instead of the model
    This is more consistent with what's done for Transactions, and probably
    better from an architecture point of view.

--- lib/qonto/actions/organization.rb
+++ lib/qonto/actions/organization.rb
@@ -3,7 +3,23 @@ module Qonto
     module Organization
       def get_organization
         response = get("/organizations/#{configuration.get(:slug)}")
-        Qonto::Response.new(response, Model::Organization.new(response.parsed_response['organization']))
+        Qonto::Response.new(response, modelize_organization(response.parsed_response['organization']))
+      end
+
+      private
+
+      def modelize_organization(organization)
+        if !organization['bank_accounts'].nil?
+          organization['bank_accounts'] = modelize_bank_accounts(organization['bank_accounts'])
+        end
+
+        Qonto::Model::Organization.new(organization)
+      end
+
+      def modelize_bank_accounts(bank_accounts)
+        bank_accounts.map do |bank_account|
+          Qonto::Model::BankAccount.new(bank_account)
+        end
       end
     end
   end
