d1d60aa70e029d6f785ad07dbf2432dc687ca3c9
Sébastien Charrier <scharrier@gmail.com>
2017-11-21 10:00:24 +0100

    Add new transaction attributes

--- lib/qonto/model/transaction.rb
+++ lib/qonto/model/transaction.rb
@@ -11,6 +11,10 @@ module Qonto
       attr_accessor :local_currency
       attr_accessor :settled_at
       attr_accessor :label
+      attr_accessor :status
+      attr_accessor :transaction_id
+      attr_accessor :emitted_at
+      attr_accessor :note
     end
   end
 end
