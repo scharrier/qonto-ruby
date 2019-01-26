8614adb1c94858bb73ff5699c73dca0d388a5258
Thomas Payet <thomas@meilisearch.com>
2019-01-26 16:13:56 +0100

    Add VAT fields in transactions

--- lib/qonto/model/transaction.rb
+++ lib/qonto/model/transaction.rb
@@ -3,18 +3,21 @@ module Qonto
     class Transaction < Base
       attr_accessor :amount
       attr_accessor :amount_cents
+      attr_accessor :currency
+      attr_accessor :emitted_at
+      attr_accessor :label
       attr_accessor :local_amount
       attr_accessor :local_amount_cents
-      attr_accessor :side
-      attr_accessor :operation_type
-      attr_accessor :currency
       attr_accessor :local_currency
+      attr_accessor :note
+      attr_accessor :operation_type
       attr_accessor :settled_at
-      attr_accessor :label
+      attr_accessor :side
       attr_accessor :status
       attr_accessor :transaction_id
-      attr_accessor :emitted_at
-      attr_accessor :note
+      attr_accessor :vat_amount
+      attr_accessor :vat_amount_cents
+      attr_accessor :vat_rate
     end
   end
 end
